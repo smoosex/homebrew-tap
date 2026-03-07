66b0a067e6f53dfee98c745386e3829531141c0f
smoose <kongzhh1101@gmail.com>
2026-03-07 12:04:18 +0800

    update echotype cask for v0.0.2

--- Casks/echotype.rb
+++ Casks/echotype.rb
@@ -1,8 +1,8 @@
 cask "echotype" do
-  version "0.0.1"
-  sha256 "5b2dc3853945dd493f81db130d349d0f4a984d13ea1197097bef3937d055669d"
+  version "0.0.2"
+  sha256 "7aef3acca75965a50d056940f903c9972afef8b5942d94aff6a45ed28aa9d84c"
 
-  url "https://github.com/smoosex/echotype/releases/download/v0.0.1/EchoType-0.0.1.dmg"
+  url "https://github.com/smoosex/echotype/releases/download/v0.0.2/EchoType-0.0.2.dmg"
   name "EchoType"
   desc "Offline speech-to-text menubar app for macOS"
   homepage "https://github.com/smoosex/echotype"
